0650fed2f94b5d198a073444f3cb30b3938c9f78
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-04-18 20:12:25 +0530

    Update cars information.cpp

--- cars information.cpp
+++ cars information.cpp
@@ -2,25 +2,25 @@
 #include<string.h>
 using namespace std;
 
-class cars{
+class car{
 	
 	public :
-		char car_id[50];
+		int car_id;
 	    char car_company_name[30];
 	    char car_colour[70];
 	    char car_model[90];   
-        char car_release_year[90];
+        int car_release_year;
 		   
 };
 int main(){
 	
-	cars obj1,obj2,obj3,obj4,obj5;
+	car obj1,obj2,obj3,obj4,obj5;
 
-	strcpy(obj1.car_id,"emp_id : 1");
+    obj1.car_id = 1;
 	strcpy(obj2.car_company_name ,"emp_company_name : toyota");
 	strcpy(obj3.car_colour,"emp_colour : blue");
 	strcpy(obj4.car_model,"emp_model : corolla");
-	strcpy(obj5.car_release_year,"emp_release_year : 2021");
+    obj5.car_release_year = 2011;
 		
 	cout<<obj1.car_id<<endl;
 	cout<<obj2.car_company_name<<endl;	
@@ -29,11 +29,11 @@ int main(){
 	cout<<obj5.car_release_year<<endl<<endl;
 	
 	
-	strcpy(obj1.car_id,"emp_id : 2");
+	obj1.car_id = 2;
 	strcpy(obj2.car_company_name ,"emp_company_name : honda");
 	strcpy(obj3.car_colour,"emp_colour : black");
 	strcpy(obj4.car_model,"emp_model : civic");
-	strcpy(obj5.car_release_year,"emp_release_year : 2022");
+	obj5.car_release_year = 2022;
 		
 	cout<<obj1.car_id<<endl;
 	cout<<obj2.car_company_name<<endl;	
@@ -41,25 +41,25 @@ int main(){
 	cout<<obj4.car_model<<endl;
 	cout<<obj5.car_release_year<<endl<<endl;
 	
-	
-	strcpy(obj1.car_id,"emp_id : 3");
+    obj1.car_id = 3;
 	strcpy(obj2.car_company_name ,"emp_company_name :ford");
 	strcpy(obj3.car_colour,"emp_colour : red");
 	strcpy(obj4.car_model,"emp_model : mustang");
-	strcpy(obj5.car_release_year,"emp_release_year : 2023");
+	obj5.car_release_year = 2023;
 		
 	cout<<obj1.car_id<<endl;
 	cout<<obj2.car_company_name<<endl;	
 	cout<<obj3.car_colour<<endl;
 	cout<<obj4.car_model<<endl;
 	cout<<obj5.car_release_year<<endl<<endl;
-	
-	strcpy(obj1.car_id,"emp_id : 4");
+
+    obj1.car_id = 4;	
 	strcpy(obj2.car_company_name ,"emp_company_name :chevrolet");
 	strcpy(obj3.car_colour,"emp_colour : silver");
 	strcpy(obj4.car_model,"emp_model : camaro");
-	strcpy(obj5.car_release_year,"emp_release_year : 2024");
+	obj5.car_release_year = 2024;
 		
+	
 	cout<<obj1.car_id<<endl;
 	cout<<obj2.car_company_name<<endl;	
 	cout<<obj3.car_colour<<endl;
@@ -67,4 +67,5 @@ int main(){
 	cout<<obj5.car_release_year<<endl<<endl;
 	
 	
+
 }
